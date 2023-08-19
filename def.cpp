1f4b7de646fc88dbf86f33d7eb1c0ecc937a92f3
ShadmanAkhter123 <shadman.akhter123@gmail.com>
2023-08-19 21:59:11 +0600

    2nd commit

--- def.cpp
+++ def.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 #include"dec.h"
 using namespace std;
+
+string board[9] = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
+int player = 1;
+int position = 0; //0-9 position
+void introduction() {
+	cout << "press [enter] or something to begin";
+	getchar(); //haults the screen or lets just say takes an input
+	cout << "\n";
+
+	cout << "\n";
+	cout << "\t*******";
+	cout << "\t   Tictactoe" << endl;
+	cout << "\t*******"<<endl;
+	cout << "Player-1 turn...X" << endl;
+	cout << "Player-2 turn...Y" << endl;
+
+	cout << "The 3X3 grid is down below\n"<<endl;
+
+	cout<< ""
+
+}
